9ce3788869c239932435a04ea6909b7e9e8f9ddf
Adam Skuta <adam.skuta@gmail.com>
2025-01-27 12:51:19 +0100

    Added CRON to vercel

--- src/app/api/energy/route.ts
+++ src/app/api/energy/route.ts
@@ -2,7 +2,15 @@ import { NextResponse } from "next/server";
 import { createEnergyData } from "~/server/db/crud";
 import type { EnergyData } from "~/server/dto/EnergyData";
 
-export async function GET() {
+export async function GET(request: Request) {
+  if (
+    request.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    return NextResponse.json(
+      { ok: false, error: "Unauthorized" },
+      { status: 401 },
+    );
+  }
   const response = await fetch("https://apis.smartenergy.at/market/v1/price");
   const { data } = (await response.json()) as { data: EnergyData[] };
   const energyData = data.map((item) => ({
@@ -10,5 +18,5 @@ export async function GET() {
     value: Math.round(item.value * 1000),
   }));
   await createEnergyData(energyData);
-  return NextResponse.json(energyData);
+  return NextResponse.json({ ok: true });
 }
