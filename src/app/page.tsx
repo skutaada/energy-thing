de41c615ebaa9384d70feca3c49e3000397738dc
skutaada <adam.skuta@gmail.com>
2025-01-27 22:16:33 +0100

    Changed root page to Vienna TZ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import EnergyCostView from "~/components/layout/EnergyCostView";
 
 export default async function Home() {
-  const currentDate = new Date();
+  const currentDate = new Date(
+    new Date().toLocaleDateString("en-US", {
+      timeZone: "Europe/Vienna",
+    }),
+  );
   return <EnergyCostView date={currentDate} />;
 }
