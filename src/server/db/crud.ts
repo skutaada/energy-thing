1fd2d4c4e66f9406e9514079d8ff61671aafa9bf
Adam Skuta <adam.skuta@gmail.com>
2025-01-27 14:32:46 +0100

    Fixed unique error checking

--- src/server/db/crud.ts
+++ src/server/db/crud.ts
@@ -2,14 +2,15 @@ import type { EnergyData } from "../dto/EnergyData";
 import { db } from ".";
 import { energyTable } from "./schema";
 import { like } from "drizzle-orm";
+import { NeonDbError } from "@neondatabase/serverless";
 
 export async function createEnergyData(data: EnergyData[]) {
   try {
     await db.insert(energyTable).values(data);
   } catch (error) {
     if (
-      error instanceof Error &&
-      !error.message.includes("UNIQUE constraint failed")
+      error instanceof NeonDbError &&
+      error.constraint !== "energy-thing_energy_table_date_unique"
     ) {
       throw error;
     }
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -4,6 +4,6 @@ export const createTable = pgTableCreator((name) => `energy-thing_${name}`);
 
 export const energyTable = createTable("energy_table", {
   id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
-  date: text("date").notNull(),
+  date: text("date").notNull().unique(),
   value: integer("value").notNull(),
 });
