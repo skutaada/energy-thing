68c262b0e3c41e3a222ff575b2b902b210a0d749
skutaada <adam.skuta@gmail.com>
2025-01-28 09:05:53 +0100

    Changed / page to dynamic for date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import EnergyCostView from "~/components/layout/EnergyCostView";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const currentDate = new Date(
     new Date().toLocaleDateString("en-US", {
